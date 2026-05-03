9bd396a1e2ec76c0365c910b4d3305ca719bb3f5
elc901 <kvadiksofficial@gmail.com>
2026-05-03 12:01:51 +0300

    незначительные изменения
    написана функция help которая будет показывать команды. Исправлен json чтобы не было языка по дефолту.

--- command.py
+++ command.py
@@ -1,27 +1,33 @@
 import json
 
-def language(write, set_handler): # выбор языка
-
-    write("Your Language (just write 'en' or 'ru') ->  ")
+def language(user_input, write):
+    with open("config.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
     
-    def handle_input(user_input):
-        with open("config.json", "r", encoding="utf-8") as f:
-            data = json.load(f)
-        
-        if user_input == "en":
-            data["language"] = "en"
-            write("Language set to English.\n")
-        elif user_input == "ru":
-            data["language"] = "ru"
-            write("Язык установлен: Русский.\n")
-        else:
-            write(f"Unknown language: '{user_input}'. Use 'en' or 'ru'.\n")
-        
-        with open("config.json", "w", encoding="utf-8") as f:
-            json.dump(data, f, indent=4, ensure_ascii=False)
+    if user_input == "en":
+        data["language"] = "en"
+        write("Language set to English.\n")
+    elif user_input == "ru":
+        data["language"] = "ru"
+        write("Язык установлен: Русский.\n")
+    else:
+        write(f"Unknown: '{user_input}'. Use 'en' or 'ru'.\n")
     
-    set_handler(handle_input)  # возврат управления в терминал
-
-
-
-def 
+    with open("config.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+def help(write):
+    with open("config.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    if data["language"] == "en":
+        write("help - show all commands\n")
+        write("clg - allows you to select the language ( ru or en )\n")
+    elif data["language"] == "ru":
+        write("help - показывает все команды\n")
+        write("clg - позволяет тебе выбрать язык ( ru или en )")
+    elif data["language"] == "none":
+        write("write 'clg'\n")
+        write("напиши 'clg'\n")
+def path_for_download_torrent(user_input, write): # дописать
+    with open("config.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    pass
--- terminal.py
+++ terminal.py
@@ -9,9 +9,9 @@ root.configure(bg="#1e1e1e")
 output = Text(root, bg="#1e1e1e", fg="#00ff00", font=("Courier", 10), bd=0)
 output.pack(fill=BOTH, expand=True)
 
-pending_handler = None  # хранит функцию, ожидающую следующего ввода
+pending_handler = None
 
-def write(text):
+def write(text): # аналог print для кастомнгоо интерфекйа
     output.insert(END, text)
     output.see(END)
 
@@ -19,32 +19,36 @@ def prompt():
     output.insert(END, ">> ")
     output.mark_set("input_start", "end-1c")
     output.mark_gravity("input_start", LEFT)
+write("help for commands    \n")
+write("help для комманд     \n")
+prompt()
 
-def set_handler(handler):
-    global pending_handler
-    pending_handler = handler
+def reset_prompt(): # обновляет позициб input_start и обновляет prompt
+    prompt()
     output.mark_set("input_start", "end-1c")
     output.mark_gravity("input_start", LEFT)
-
-prompt()
 
 def run(event):
     global pending_handler
     line = output.get("input_start", "end-1c").strip()
     output.insert(END, "\n")
-
     if pending_handler:
-       #передача ввода
-        handler = pending_handler
+        pending_handler(line, write)
         pending_handler = None
-        handler(line)
         prompt()
     elif line == "clear":
         output.delete(1.0, END)
         prompt()
-    elif line == "clg":
-        command.language(write, set_handler)
-        
+    elif line == "clg": # выбор языка
+        write("Your Language (just write 'en' or 'ru') ->  ")
+        output.mark_set("input_start", "end-1c")
+        output.mark_gravity("input_start", LEFT)
+        pending_handler = command.language
+    elif line == "help":
+        command.help(write)
+        reset_prompt()
+    elif line == "exit": # выход
+        exit()
     else:
         write(f"unknown command: {line}\n")
         prompt()
