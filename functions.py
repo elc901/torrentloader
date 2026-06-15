13239a0a6e0e2bfc46d3029bcf09bd0b334821fc
elc901 <kvadiksofficial@gmail.com>
2026-06-15 13:59:50 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,14 +1,35 @@
 import customtkinter as ctk
-import tkinter as tk
-ctk.set_appearance_mode("light")
-ctk.set_default_color_theme("blue")
+import json
+#with open("settings.json", "r") as f:
+    #settings = json.load(f)
+
+#with open("settings.json", "w") as f:
+    #json.dump()
+open_window = None  
+
+def on_close():
+    global open_window
+    if open_window is not None:
+        open_window.destroy()
+        open_window = None
+
 def open_settings_wdw(parent):
-    setting_wdw = ctk.CTkToplevel(parent) 
-    setting_wdw.title("Settings")
-    setting_wdw.geometry("300x450")
+    global open_window
     
-    label = ctk.CTkLabel(setting_wdw, text="Settings", font=("Montserrat", 16))
-    label.pack(pady=20)
     
-    close_btn = ctk.CTkButton(setting_wdw, text="Close", command=setting_wdw.destroy)
-    close_btn.pack(pady=10)
+    if open_window is not None and open_window.winfo_exists():
+        open_window.lift()
+        open_window.focus()
+        return
+    
+    
+    open_window = ctk.CTkToplevel(parent)  
+    open_window.title("Settings")
+    open_window.geometry("300x450+100+200")
+    
+    
+    open_window.protocol("WM_DELETE_WINDOW", on_close)
+    
+    
+    close_btn = ctk.CTkButton(open_window, text="Save and close", command=on_close)
+    close_btn.place(x=75, y=420)  
--- interface.py
+++ interface.py
@@ -1,14 +1,14 @@
 import customtkinter as ctk
 from functions import *
 
-# Настройка темы
-ctk.set_appearance_mode("light")  # или "dark"
+
+ctk.set_appearance_mode("light")  
 ctk.set_default_color_theme("blue")
 
-# Главное окно
+# главное окно
 root = ctk.CTk()
 root.title("Tray")
-root.geometry("900x600")
+root.geometry("900x600+500+200")
 
 settings_btn = ctk.CTkButton(
     root,
@@ -17,7 +17,9 @@ settings_btn = ctk.CTkButton(
     text_color="white",    # вместо fg
     font=("Montserrat", 10),
     command=lambda: open_settings_wdw(root),
-    cursor="hand2"
+    cursor="hand2",
+    width=10,
+    height=10
 )
 settings_btn.place(x=1, y=1)
 
